c429684ce16256748c2fcc636c3b773623303ca2
caoyanyuan <1689456277@qq.com>
2019-06-19 17:05:35 +0800

    JS/Class： ImageUpload   AsyncTaskArray

---
+++ JS/class/AsyncTaskArray.js
@@ -0,0 +1,83 @@
+import { isFunction, isNullOrUndefined } from "common/js/utils";
+
+/* 
+    解决多个异步问题,  在imgUpload有调用
+    let tasks = new AsyncTaskArray()
+    异步调用时，有几个异步调用几个
+    tasks.add()
+    异步结束后执行  
+    tasks.end()
+    一个异步结束。任务数量减掉1
+    tasks.check()
+*/
+export default class AsyncTaskArray {
+  _tasksNum;
+  _err = { name: "taskList运行错误", message: undefined };
+
+  _callbackFunc = null;
+
+  _callbackParams;
+
+  constructor() {
+    this._tasksNum = 0;
+    return this;
+  }
+
+  set(params) {
+    this._callbackParams = params;
+    return this;
+  }
+
+  end(callback) {
+    this._callbackFunc = callback;
+    return this;
+  }
+
+  check(error) {
+    this._tasksNum--;
+    this._setErrorMessage(error);
+    
+    if (this._tasksNum === 0) {
+      this._callback();
+    }
+  }
+
+  add(func) {
+    if (func) {
+      setTimeout(() => {
+        if (isFunction(func)) {
+          func();
+        }
+      }, 10);
+    }
+    this._tasksNum++;
+    return this;
+  }
+
+  _setErrorMessage(error) {
+    if (isNullOrUndefined(error) === false) {
+      if (this._err.message !== null) {
+        if (error) {
+          this._err.message += "," + error.message;
+        }
+      } else {
+        if (error) {
+          this._err.message = error.message;
+        }
+      }
+    }
+  }
+
+  _callback() {
+    if (!isNullOrUndefined(this._err.message)) {
+      if (this._callbackFunc) {
+        this._callbackFunc(this._err, this._callbackParams);
+      }
+    } else {
+       
+      if (this._callbackFunc) {
+        this._callbackFunc(null, this._callbackParams);
+      }
+    }
+  }
+}
---
+++ JS/class/imgUpload.js
@@ -0,0 +1,230 @@
+import httpRequest from '@/utils/httpRequest'  //封装的请求含糊
+import { proxyUrl } from '@/api/fetch'    //拼出接口路径 在正式和本地环境才开代理要加/proxyApi   在测试环境不用开代理
+import { getPicture } from '@/api/api'   //拼出绝对路径
+import AsyncTaskArray from 'common/js/AsyncTaskArray'  
+
+const _resolve = Symbol( 'resolve' )
+const _reject = Symbol( 'reject' )
+
+const DEFAULT_TYPES = ['image/jpeg', 'image/png', 'image/png', 'image/jpg']
+const UPLOAD_URL = proxyUrl('/file/img')   //{file}
+
+class ImgUpload {
+    /**
+     * 
+     * @param {Array}  fileTypes 文件类型  image/jpeg等
+     * @param {Boolean} multiple 是否为多选  
+     * @param {Number} size 文件大小(M)
+     * @param {String} ratio 图片比例 1:1
+     * @param {String} precise 固定图片宽高  100*100
+     */
+  
+    constructor({fileTypes = DEFAULT_TYPES, multiple=false, size=2, ratio, precise }) {
+        Object.assign(this, {
+            fileTypes, multiple, size, ratio, precise
+        })
+
+        this.promise = new Promise((resolve, reject) => {
+            this[ _resolve ] = resolve
+            this[ _reject ] = reject
+        })
+
+        this.images = []
+        this.imageElems = []
+    }
+
+    chooseImage() {
+        let input = document.createElement("input");
+        
+			input.type = "file";
+			input.value = "";
+            input.multiple = this.multiple ? true : false
+			input.click();
+
+		input.addEventListener("change", () => {
+
+			this.fileList = Array.from(input.files);
+            this.check().then(() => {
+                this.fileList.map((fileItem, index) => {
+
+                    let file = new FormData()
+                    file.append("file", fileItem)
+
+                    httpRequest.post(UPLOAD_URL, file).then(res => {
+                        
+                        if (res.data.code == 0) {
+                            if(!this.multiple) {
+                                this.images = []
+                            }
+                            let url = res.data.data
+                            this.images.push({
+                                url,
+                                completeUrl: getPicture(url)
+                            })
+                            //this.getBlob(fileItem, index) 
+                            this[ _resolve ](this.images)
+                        } else {
+                            this[ _reject ](res.data.msg)
+                        }
+                    });
+                })
+            })
+			
+		});
+    }
+
+    _getReader() {
+        if(!!window.FileReader) {
+            return new FileReader()
+        }else{
+            console.log("您的设备不支持图片预览功能，如需该功能请升级您的设备！");
+        }
+    }
+    
+    /**本地预览功能 暂时没用到*/
+    getBlob(fileItem, index) {
+        let self = this
+        let fReader = this._getReader()()
+        
+        fReader.onload = function(e) {
+            let url = reader.result;
+            self.images[index].blob = url
+            
+            self[ _resolve ](self.images)
+        };
+        fReader.readAsDataURL(fileItem);
+    }
+    
+    /**检验文件 
+     * 1.文件类型 2.文件大小  (同步)
+     * 3.图片宽高比例 4.图片精确宽高  (异步)
+     * */
+    check() {
+        let isLtM = true, 
+            isType = true, 
+            msg = null
+        
+        
+        this.fileList.every(file => {
+
+            if (file.size / 1024 / 1024 > this.size) {
+                isLtM = false
+            }
+
+            isType = this.fileTypes.some(type => file.type === type)
+        })
+
+        if ( !isType ) {
+            msg = `只能上传${this.fileTypes.join(',')}文件!`
+        }
+        if ( !isLtM ) {
+            msg = `图片大小不能超过${this.size}MB!`
+        }
+        
+        return new Promise((resolve, reject) => {
+            if(msg) {
+                this[ _reject](msg)
+
+            }else{
+                let hasPromise = this.ratio || this.precise     //是否有异步检验项
+    
+                if( hasPromise ) {
+                    this.tasks = new AsyncTaskArray()
+    
+                    this.fileList.map((file,index) => {
+                        this.tasks.add()
+                        
+                        this.getImageWidth(file,index).then(({width, height}) => {
+                            let isRatio,isPrecise = true
+                            if(this.ratio) {
+                                isRatio = this.checkRatio({width, height})
+                            }
+                            if(this.precise) {
+                                isPrecise = this.checkPrecise({width, height})
+                            }
+    
+                            if(isRatio && isPrecise) {  //等每一张图片都通过验证。该项任务就结束了
+                                this.tasks.check()
+                            }
+                        })
+                        
+                        //所有的图片都通过验证
+                        this.tasks.end(() => { 
+                            resolve()
+                        })
+                    })
+
+                } else {
+                    resolve()
+                }
+            }
+        })
+       
+        
+    }
+
+
+    checkRatio({width, height}) {
+        let values = this.ratio.split(':')
+        
+        if(width/height == values[0]/values[1]) {
+            return true
+        }else{
+            this[_reject](`请上传比例为${values[0]}:${values[1]}的图片`)
+        }
+    }
+
+    checkPrecise({width, height}) {
+        let values = this.precise.split('*')
+        
+        if(width == values[0] && height == values[1]) {
+            return true
+        }else{
+            this[_reject](`请上传为${values[0]}*${values[1]}的图片`)
+        }
+    }
+
+    /**
+     * 
+     * @param {file} file 上传的file对象
+     */
+    getImageWidth(file,index) {
+        return new Promise((resolve, reject) => {
+            
+            if(this.imageElems && this.imageElems[index]) {
+                resolve(this.imageElems[index])
+                return 
+            }
+
+            let fReader = this._getReader()
+           
+            fReader.onloadend = () => {
+                let image = new Image(),
+                    url = fReader.result;
+                
+                if (typeof url === "string") {
+                    image.src = url;
+                }
+                
+                image.onload = () => {
+                    let obj = {
+                        index,
+                        file,
+                        width: image.width,
+                        height: image.height
+                    }
+                    this.imageElems.push(obj)
+                    
+                    resolve(obj);
+                };
+            };
+            fReader.readAsDataURL(file);
+            
+        });
+    }
+
+    
+
+}
+
+export default ImgUpload
