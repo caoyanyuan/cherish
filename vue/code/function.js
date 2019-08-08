05fc7ee294a6c5052efdebb768fb305dbd3ebaa8
caoyanyuan <1689456277@qq.com>
2019-08-08 17:01:12 +0800

    vue 全局 局部刷新 巧用

--- vue/code/function.js
+++ vue/code/function.js
@@ -1,4 +1,4 @@
-//隐藏所有 tool-item下拉框组件
+//1. 隐藏所有 tool-item下拉框组件
 //child.$options.name 获取组件名称
 function handleParentClick() {
 
@@ -9,4 +9,22 @@ function handleParentClick() {
             item.hide()
         })
     }
+}
+
+
+//2. 全局 局部刷新 巧用
+/*
+<wrapper class="wrapper">
+    <!-- 三级级页面 -->
+    <keep-alive v-if="toggleRouterVisible">
+        <router-view></router-view>
+    </keep-alive>
+</wrapper>
+*/
+
+function refreshRouter() {
+    this.toggleRouterVisible = false
+    setTimeout(() => {
+        this.toggleRouterVisible = true
+    })
 }
