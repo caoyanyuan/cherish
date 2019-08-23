5a49224cd2f3175e53e5420a4fb680416511ae43
caoyanyuan <1689456277@qq.com>
2019-08-23 09:35:21 +0800

    位运算：数组arr 排列可能所有可能的组合 n个及n个以上

---
+++ JS/utils/bitOperation.js
@@ -0,0 +1,32 @@
+// 对数组arr 排列可能所有可能的组合 n个及n个以上
+export function permutationAndCombination(arr, n){
+
+    let ret = [],
+        _len = arr.length;
+
+    for (let m=n || 0; m <= _len; m++) {
+
+        for (let i=0;i< Math.pow(2,_len);i++){
+            let a = 0,
+                sum = 0,
+                sumIndex = []
+
+            for (let j=0;j<_len;j++){
+                if(i>>j & 1){
+                    a++;
+                    sumIndex.push(j);
+                    sum += arr[j]
+                }
+            }
+
+            if(a==m){
+                ret.push({
+                    sum,
+                    sumIndex
+                });
+            }
+        }
+    }
+
+    return ret;
+}
